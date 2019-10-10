07f2d439447b6892ab716b00d6e9237263b5c0b5
K-Lakpriya <k.lakpriya96@gmail.com>
2019-10-11 01:18:51 +0530

    verify user

---
+++ results.php
@@ -0,0 +1,119 @@
+<?php
+if(isset($_POST['username'],$_POST['password'])){
+	$uname = $_POST['username'];
+	$pwd = $_POST['password'];
+	if($uname == 'admin' && $pwd == 'admin'){
+		echo '<h1>Successfully logged in</h1>';
+		session_start();
+		$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+		$session_id = session_id();
+		setcookie('sessionCookie',$session_id,time()+ 60*60*24*365 ,'/');
+		setcookie('csrfTokenCookie',$_SESSION['token'],time()+ 60*60*24*365 ,'/');
+		
+	}else{
+		echo '<h1>Invalid Credentials</h1>';
+		exit();
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cross Site Request Forgery Protection</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--===============================================================================================-->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script>
+	
+	$(document).ready(function(){
+	
+	var cookie_value = "";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+	var csrf = decodedCookie.split(';')[2]
+	// alert(decodedCookie)
+	if(csrf.split('=')[0] = "csrfTokenCookie" ){
+		// alert(csrf.split('csrfTokenCookie=')[1])
+		cookie_value = csrf.split('csrfTokenCookie=')[1];
+		document.getElementById("tokenIn_hidden_field").setAttribute('value', cookie_value) ;
+	}
+	});
+
+</script>
+
+  <head>
+	<body>
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+				Double Submit Cookies Pattern
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5" action="home.php" method="POST">
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="updatepost" id="username" placeholder="Write a Message">
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+
+					<div>
+						<input type="hidden" name="token" id="tokenIn_hidden_field">				
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn" type="submit" value="updatepost">
+							Submit
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+
+	</body> 
+</html>
