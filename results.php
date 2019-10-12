3f01ad73c730c4a2425165d582c16a46ded42882
Kanchana Lakpriya <k.lakpriya96@gmail.com>
2019-10-12 11:40:24 +0530

    Error fix

--- results.php
+++ results.php
@@ -7,8 +7,8 @@ if(isset($_POST['username'],$_POST['password'])){
 		session_start();
 		$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
 		$session_id = session_id();
-		setcookie('sessionCookie',$session_id,time()+ 60*60*24*365 ,'/');
-		setcookie('csrfTokenCookie',$_SESSION['token'],time()+ 60*60*24*365 ,'/');
+		setcookie('sessionCookie',$session_id,time()+ 60*60*24*365 ,'/',"",false,false);
+		setcookie('csrfTokenCookie',$_SESSION['token'],time()+ 60*60*24*365 ,'/',false,false);
 		
 	}else{
 		echo '<h1>Invalid Credentials</h1>';
